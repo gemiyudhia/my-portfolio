cd0f79d0a0f7df82ecbc6676274079aa34190eb6
Gemi <githuba122@gmail.com>
2024-10-11 11:19:30 +0700

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { faClock } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <FontAwesomeIcon icon={faClock} />
+    </>
+  );
+}
+
+export default App
