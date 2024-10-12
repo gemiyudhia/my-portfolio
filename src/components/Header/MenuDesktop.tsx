c9e35d512911dde74b45035480835b62a248a41d
Gemi <githuba122@gmail.com>
2024-10-12 14:01:43 +0700

    feat(navbar): make menu to component

---
+++ src/components/Header/MenuDesktop.tsx
@@ -0,0 +1,78 @@
+type MenuDesktopProps = {
+  activeMenu: string;
+  handleMenuClick: (
+    menu: string,
+    event: React.MouseEvent<HTMLAnchorElement>
+  ) => void;
+};
+
+const MenuDesktop = ({ activeMenu, handleMenuClick }: MenuDesktopProps) => {
+  return (
+    <div className="hidden md:flex items-center font-semibold text-lg text-white md:ml-auto lg:ml-0 lg:justify-center lg:w-full">
+      <ul className="flex items-center md:gap-x-4 lg:gap-x-32 lg:text-xl">
+        <li>
+          <a
+            href="/"
+            className={`hover:text-gray-300 lg:py-5 lg:px-8 md:py-4 md:px-5 rounded-full transition-all duration-300 ease-in-out ${
+              activeMenu === "home"
+                ? " hover:text-secondary text-secondary bg-white"
+                : ""
+            }`}
+            onClick={(event) => handleMenuClick("home", event)}
+          >
+            Home
+          </a>
+        </li>
+        <li>
+          <a
+            href="/"
+            className={`hover:text-gray-300 lg:py-5 lg:px-8 md:py-4 md:px-5 rounded-full transition-all duration-300 ease-in-out ${
+              activeMenu === "about"
+                ? "hover:text-secondary text-secondary bg-white"
+                : ""
+            }`}
+            onClick={(event) => handleMenuClick("about", event)}
+          >
+            About
+          </a>
+        </li>
+
+        {/* Logo di tengah untuk layar besar */}
+        <li>
+          <h1 className="hidden lg:block bg-white text-secondary font-black p-4 text-2xl rounded-full">
+            GY
+          </h1>
+        </li>
+
+        <li>
+          <a
+            href="/"
+            className={`hover:text-gray-300 lg:py-5 lg:px-8 md:py-4 md:px-5 rounded-full transition-all duration-300 ease-in-out ${
+              activeMenu === "myworks"
+                ? "hover:text-secondary text-secondary bg-white"
+                : ""
+            }`}
+            onClick={(event) => handleMenuClick("myworks", event)}
+          >
+            My Works
+          </a>
+        </li>
+        <li>
+          <a
+            href="/"
+            className={`hover:text-gray-300 lg:py-5 lg:px-8 md:py-4 md:px-5 rounded-full transition-all duration-300 ease-in-out ${
+              activeMenu === "contact"
+                ? "hover:text-secondary text-secondary bg-white"
+                : ""
+            }`}
+            onClick={(event) => handleMenuClick("contact", event)}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default MenuDesktop;
---
+++ src/components/Header/MenuMobile.tsx
@@ -0,0 +1,32 @@
+type MenuMobileProps = {
+  isOpen: boolean;
+}
+
+const MenuMobile = ({isOpen}: MenuMobileProps) => {
+  return (
+    <div
+      className={`absolute bg-secondary-0 w-[93%] right-5 top-24 py-4 px-3 rounded-lg transition-all duration-300 ease-in-out transform md:hidden ${
+        isOpen
+          ? "translate-y-0 opacity-100"
+          : "-translate-y-10 opacity-0 pointer-events-none"
+      }`}
+    >
+      <ul className="block bg-primary mt-5 py-7 px-4 text-white text-lg font-semibold rounded-xl space-y-4">
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">My Works</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default MenuMobile
