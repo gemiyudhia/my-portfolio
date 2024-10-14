e410c980aba81162f2a0c35c81e0bfdc13b82e3e
Gemi <githuba122@gmail.com>
2024-10-14 20:38:13 +0700

    feat: add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,17 @@
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="mt-28">
+      <div className=" bg-secondary pt-10 pb-14 text-center">
+        <h1 className="bg-white text-secondary font-black p-4 text-2xl rounded-full w-20 h-20 mx-auto flex items-center justify-center mb-3">
+          GY
+        </h1>
+        <p className="text-white font-medium text-sm">
+          © 2024 Gemi Yudhia | Frontend Developer
+        </p>
+        <p className="text-white font-medium text-sm">All Rights Reserved.</p>
+      </div>
+    </section>
+  );
 }
 
 export default Footer
