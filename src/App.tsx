452c359cfe6a5614513d9636079fe54667428e42
Gemi <githuba122@gmail.com>
2024-10-16 20:08:58 +0700

    feat: add id

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Footer from "./components/Footer/Footer";
+import FormContact from "./components/FormContact";
 import Header from "./components/Header";
+import HeroSection from "./components/Hero";
+import Projects from "./components/Projects";
 
 function App() {
   return (
-    <div className="container mx-auto px-5 py-8">
-      <Router>
+    <>
+      <div className="container mx-auto px-5 py-8">
         <Header />
-        <Routes>
-          <Route index path="/" element="" />
-        </Routes>
-      </Router>
-    </div>
+        <section id="home">
+          <HeroSection />
+        </section>
+        <section id="about">
+          <About />
+        </section>
+        <section id="project">
+          <Projects />
+        </section>
+        <section id="contact">
+          <FormContact />
+        </section>
+      </div>
+      <Footer />
+    </>
   );
 }
 
